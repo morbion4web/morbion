9598ad963f166ad8a0921ab15d5a84e3ca103996
morbion4web <90639725+morbion4web@users.noreply.github.com>
2021-09-13 21:44:12 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Morbion Inc.</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div id="box">
+    <div id="item">
+      Hello,
+    </div>
+    <div id="item">
+      Morbion team!
+    </div>
+  </div>
+</body>
+</html>
